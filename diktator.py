c88838c98b2ce45a9b510e9f444230bae2cce923
mimikri <pocco_13@yahoo.de>
2024-01-31 15:18:23 +0100

    split 4 process tha mic process, sends the audio to the main process wich puts it into the stt queue this forms the string and puts it into the commandqueue and puts the successmessage to the tts queue some changes for commands in scconfig.py processes for dictator

--- diktator.py
+++ diktator.py
@@ -1,31 +1,74 @@
-# -*- coding: utf-8 -*-
+#!./venv/bin/python3
+
+#3 processes
+#stt
+#microphone
+#main
 import speech_recognition as sr
-import pyttsx3
-import pyautogui
-import time
-import json
-import keyboard
-import os
 from pynput.keyboard import Key, Controller
-
+import json
 keyboard = Controller()
-# Initialize the speech engine
-engine = pyttsx3.init()
-engine.setProperty('voice', 'german')  # Use the appropriate voice for German language
-os.system("setxkbmap -layout de_nodeadkeys")
+import multiprocessing
+audiocount = 0
+audioall = 0
+
+r = sr.Recognizer()
+r.energy_threshold = 1500  # minimum audio energy to consider for recording
+r.dynamic_energy_threshold = True
+r.dynamic_energy_adjustment_damping = 0.15
+r.dynamic_energy_ratio = 1.5
+r.pause_threshold = 0.05  # seconds of non-speaking audio before a phrase is considered complete
+r.operation_timeout = None  # seconds after an internal operation (e.g., an API request) starts before it times out, or ``None`` for no timeout
+r.phrase_threshold = 0.05  # minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
+r.non_speaking_duration = 0.05  # seconds of non-speaking audio to keep on both sides of the recording
 
 
+#___command interpretation and execution process_____________________________________________________________________________________________
+
+# Start the command execution process
+commands_queue = multiprocessing.Queue()
+
+def command_execution_process(commands_queue):
+
+    while True:
+        voicestring = commands_queue.get()  # Wait for a recognized voice command
+        keyboard.type(voicestring + " ")
+
+# Start the command execution process
+command_execution_process = multiprocessing.Process(target=command_execution_process, args=(commands_queue,))
+command_execution_process.start()
+
+#____microphone process____________________________________________________________________________________________
+
+# Define a queue to pass the recognized voice strings from the microphone process to the processing process
+audio_queue = multiprocessing.Queue()
+
+# Function to handle microphone input and recognition
+def microphone_recognition_process(audio_queue,r):
+    with sr.Microphone(chunk_size=1024) as source:
+        while True:
+            
+            audio_queue.put(r.listen(source))
+
+# Start the microphone recognition process
+microphone_process = multiprocessing.Process(target=microphone_recognition_process, args=(audio_queue,r))
+microphone_process.start()
+
+
+#____main loop____________________________________________________________________________________________
+
+# Inside the processing logic
 while True:
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print("Ready for order")
-        audio = r.listen(source)
-    try:
-        result = r.recognize_vosk(audio, language="de_DE")
-        recognized_text = json.loads(result)["text"]
-        print("Recognized:", recognized_text)
-        keyboard.type(recognized_text)
-    except sr.UnknownValueError:
-        print("Could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results")
+    # Check if there's a recognized voice string in the queue
+    if not audio_queue.empty():
+        audio = audio_queue.get()
+        # Process the recognized voice string
+        recognized_text = json.loads(r.recognize_vosk(audio, language="de"))["text"].strip()
+        audioall = audioall +1
+        if recognized_text != "":
+            audiocount = audiocount +1
+            print(f'[{audiocount}/{audioall}] {recognized_text}')
+            commands_queue.put(recognized_text) 
+        
+           
+
--- stringinterpreter.py
+++ stringinterpreter.py
@@ -1,39 +1,48 @@
 #!./venv/bin/python3
 #venv/bin/pip3 install speechrecognition pyttsx4 pyautogui pynput pyaudio vosk multiprocessing time
-
+#4 processes
+#tts
+#stt
+#microphone
+#main
 import speech_recognition as sr
 import pyttsx4
 import pyautogui
 import time
 from pynput.keyboard import Key, Controller
+import json
 keyboard = Controller()
-import pygame
 import multiprocessing
-# Initialize the speech engine
-engine = pyttsx4.init('espeak')
-engine.setProperty('voice', 'german')  # Use the appropriate voice for German language
-engine.setProperty('rate', 150)
-engine.setProperty('pitch', 0.5)
-voices = engine.getProperty('voices')
-
-#for voice in engine.getProperty('voices'):
-#    print(voice)
-engine.say("aktiviere sprachassistenten")
-engine.runAndWait()
+audiocount = 0
+audioall = 0
 # Define the array of dictionaries
 import scconfig
 commands = scconfig.commands
+r = sr.Recognizer()
+r.energy_threshold = 1500  # minimum audio energy to consider for recording
+r.dynamic_energy_threshold = True
+r.dynamic_energy_adjustment_damping = 0.15
+r.dynamic_energy_ratio = 1.5
+r.pause_threshold = 0.05  # seconds of non-speaking audio before a phrase is considered complete
+r.operation_timeout = None  # seconds after an internal operation (e.g., an API request) starts before it times out, or ``None`` for no timeout
+r.phrase_threshold = 0.05  # minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
+r.non_speaking_duration = 0.05  # seconds of non-speaking audio to keep on both sides of the recording
 
+#___tts successmessage process_____________________________________________________________________________________________
 
 def sound_process(sound_queue):
-    pygame.mixer.init()  # Initialize Pygame's mixer
-    recognition_sound_path = 'gotit.wav'
-    sound_queue.put(recognition_sound_path)
+        # Initialize the speech engine
+    engine = pyttsx4.init('espeak')
+    engine.setProperty('voice', 'german')  # Use the appropriate voice for German language
+    engine.setProperty('rate', 150)
+    engine.setProperty('pitch', 0.5)
+    engine.say("aktiviere sprachassistenten")
+    engine.runAndWait()
     while True:
         sound_path = sound_queue.get()  # Wait for a sound path to play
         if sound_path:
-            sound = pygame.mixer.Sound(sound_path)
-            sound.play()
+            engine.say(sound_path)
+            engine.runAndWait()          
 
 # Start the sound process
 recognition_sound_path = 'gotit.wav'
@@ -41,31 +50,37 @@ sound_queue = multiprocessing.Queue()
 sound_process = multiprocessing.Process(target=sound_process, args=(sound_queue,))
 sound_process.start()
 
-def command_execution_logic(command, voicestring, sound_queue):
-    if command["order_string"] in voicestring.lower():
-        print(command["key_to_press"])
-        if "_" in command["key_to_press"]:
-            if command["key_to_press"] == "maus_left":
-                click_both_buttons(3)
-            keys = command["key_to_press"].split("_")
-            pyautogui.hotkey(*keys)
-            if command["order_string"] == "info":
-                pyautogui.typewrite("r_DisplayInfo 3")
-                pyautogui.hotkey("enter")
-                pyautogui.hotkey("shiftright", "^")
-        else:
-            pyautogui.press(command["key_to_press"])
-        sound_queue.put('gotit.wav')
+
+#___command interpretation and execution process_____________________________________________________________________________________________
 
 # Start the command execution process
 commands_queue = multiprocessing.Queue()
 
 def command_execution_process(sound_queue, commands_queue):
+
     while True:
         voicestring = commands_queue.get()  # Wait for a recognized voice command
         for command in commands:
-            command_execution_logic(command, voicestring, sound_queue)
-
+            if command["order_string"] in voicestring.lower():
+                print('command:', command["success_message"], ' key_to_press:', command["key_to_press"])
+                try:
+                    if "_" in command["key_to_press"]:
+                        if command["key_to_press"] == "maus_left":
+                            click_both_buttons(3)
+                        keys = command["key_to_press"].split("_")
+                        pyautogui.hotkey(*keys)
+                        if command["order_string"] == "info":
+                            pyautogui.typewrite("r_DisplayInfo 3")
+                            pyautogui.hotkey("enter")
+                            pyautogui.hotkey("shiftright", "^")
+                    else:
+                        if command["key_type"] == "special":
+                            pyautogui.press(command["key_to_press"])
+                        else:
+                            keyboard.type(command["key_to_press"])
+                    sound_queue.put(command['success_message'])
+                except Exception as e:
+                    print(e)
 # Start the command execution process
 command_execution_process = multiprocessing.Process(target=command_execution_process, args=(sound_queue, commands_queue))
 command_execution_process.start()
@@ -73,53 +88,57 @@ command_execution_process.start()
 
 
 
+#____fire function____________________________________________________________________________________________
 
 
 
 
 def click_both_buttons(duration):
-    # Simulate pressing both left and right mouse buttons at the current mouse position
     initial_x, initial_y = pyautogui.position()
-
-    # Simulate pressing both left and right mouse buttons at the initial position
     pyautogui.mouseDown(button='left')
     pyautogui.mouseDown(button='right', x=initial_x, y=initial_y)
-
-    # Wait for the specified duration
     time.sleep(duration)
-
-    # Capture the updated mouse position
     updated_x, updated_y = pyautogui.position()
-
-    # Release both left and right mouse buttons at the updated position
     pyautogui.mouseUp(button='left', x=updated_x, y=updated_y)
     pyautogui.mouseUp(button='right', x=updated_x, y=updated_y)
 
 
+#____microphone process____________________________________________________________________________________________
+
+# Define a queue to pass the recognized voice strings from the microphone process to the processing process
+audio_queue = multiprocessing.Queue()
+
+# Function to handle microphone input and recognition
+def microphone_recognition_process(audio_queue,r):
+    with sr.Microphone(chunk_size=1024) as source:
+        while True:
+            
+            audio_queue.put(r.listen(source))
+
+# Start the microphone recognition process
+microphone_process = multiprocessing.Process(target=microphone_recognition_process, args=(audio_queue,r))
+microphone_process.start()
 
 
+#____main loop____________________________________________________________________________________________
 
-
+# Inside the processing logic
 while True:
-    r = sr.Recognizer()
-    r.energy_threshold = 300 # minimum audio energy to consider for recording
-    r.dynamic_energy_threshold = True
-    r.dynamic_energy_adjustment_damping = 0.15
-    r.dynamic_energy_ratio = 1.5
-    r.pause_threshold = 0.2  # seconds of non-speaking audio before a phrase is considered complete
-    r.operation_timeout = None  # seconds after an internal operation (e.g., an API request) starts before it times out, or ``None`` for no timeout
+    # Check if there's a recognized voice string in the queue
+    if not audio_queue.empty():
+        audio = audio_queue.get()
+        # Process the recognized voice string
+        recognized_text = json.loads(r.recognize_vosk(audio, language="de"))["text"].strip()
+        audioall = audioall +1
+        if recognized_text != "":
+            audiocount = audiocount +1
+            print(f'[{audiocount}/{audioall}] {recognized_text}')
+            commands_queue.put(recognized_text) 
+        
+           
 
-    r.phrase_threshold = 0.2  # minimum seconds of speaking audio before we consider the speaking audio a phrase - values below this are ignored (for filtering out clicks and pops)
-    r.non_speaking_duration = 0.2  # seconds of non-speaking audio to keep on both sides of the recording
-    with sr.Microphone(chunk_size=8192) as source:
-        print("ready for order")
-        audio = r.listen(source)
-    try:
-        commands_queue.put(r.recognize_vosk(audio, language="de"))
-    except sr.UnknownValueError:
-        print("could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results")
+
+
 
 
 
